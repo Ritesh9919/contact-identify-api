d2a094724fe9aaadc1037967bab36a91dc9f770e
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-07-21 12:03:45 +0530

    add Contact model with Sequelize integration

---
+++ models/contact.model.ts
@@ -0,0 +1,62 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/db";
+
+class Contact extends Model {
+  public id!: number;
+  phoneNumber!: string | null;
+  public email!: string | null;
+  public linkedId!: number | null;
+  public linkPrecedence!: "primary" | "secondary";
+  public createdAt!: Date;
+  public updatedAt!: Date;
+  public deletedAt!: Date | null;
+}
+
+Contact.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    linkedId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    linkPrecedence: {
+      type: DataTypes.ENUM("primary", "secondary"),
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Contact",
+    tableName: "contacts",
+    paranoid: true,
+    timestamps: true,
+  }
+);
+
+export default Contact;
